8ae864a0cd4ede4495db21273a402ddae9423967
Emper5 <tni.gabriel@gmail.com>
2022-10-22 23:50:54 +0100

    Add service methods and test in search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -7,7 +8,7 @@ import { HopiumfolioModule } from './hopiumfolio/hopiumfolio.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HopiumfolioModule, AppRoutingModule],
+  imports: [BrowserModule, HopiumfolioModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
