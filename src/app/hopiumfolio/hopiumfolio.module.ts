b3b56bc0af730d1aee515a98769dc54f0b2cadc3
Emper5 <tni.gabriel@gmail.com>
2022-10-23 16:50:10 +0100

    first aproach of search

--- src/app/hopiumfolio/hopiumfolio.module.ts
+++ src/app/hopiumfolio/hopiumfolio.module.ts
@@ -4,9 +4,17 @@ import { SearchComponent } from './components/search/search.component';
 import { TableComponent } from './components/table/table.component';
 import { HopiumfolioComponent } from './hopiumfolio.component';
 import { HopiumRoutingModule } from './hopiumfolio-routing.module';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [SearchComponent, TableComponent, HopiumfolioComponent],
-  imports: [CommonModule, HopiumRoutingModule],
+  imports: [CommonModule, HopiumRoutingModule, MatAutocompleteModule, MatFormFieldModule, ReactiveFormsModule,
+    MatInputModule, FormsModule, MatButtonModule, MatIconModule]
 })
 export class HopiumfolioModule {}
