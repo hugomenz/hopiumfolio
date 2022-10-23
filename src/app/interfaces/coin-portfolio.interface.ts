69e16fc81fa91470b333e34b38592f32cf7f7d26
Hugo Martin <hugomartin.menz@gmail.com>
2022-10-23 23:36:45 +0200

    Second approach mat table

--- src/app/interfaces/coin-portfolio.interface.ts
+++ src/app/interfaces/coin-portfolio.interface.ts
@@ -4,3 +4,13 @@ export interface CoinPortfolio {
   current_price: number;
   market_cap: number;
 }
+
+export interface RowInfo {
+  logoFirst: string;
+  cryptoFirst: string;
+  logoSecond: string;
+  cryptoSecond: string;
+  multiply: number;
+  price: number;
+  gain: number;
+}
